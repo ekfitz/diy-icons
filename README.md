363b932f97353dcb7edc1e9d65ee584f70cc20ae
Eric <57112072+ekfitz@users.noreply.github.com>
2019-11-09 19:37:45 -0700

    kind of works

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ For the hamburger, the id is "hamburger". Set the style of this.
 All class is "hamnav". Set the color and opacity of this.
 
 Scratch that, changing the lines to the class name to easily change their style. The whole does not have a class name.
+
+Probably going to style icon by id rather than class
--- js/hamburger.js
+++ js/hamburger.js
@@ -1,32 +1,54 @@
 hamNav();
 
+var hamburger = document.getElementById("hamburger");
+var topLine = document.getElementById("topLine");
+var middleLine = document.getElementById("middleLine");
+var bottomLine = document.getElementById("bottomLine");
+
+hamburger.style.backgroundColor = "green";
+
+setColors();
+
 function hamNav() {
   var ham = document.createElement("div");
   var tl = document.createElement("span");
   var ml = document.createElement("span");
   var bl = document.createElement("span");
 
-  ham.className = "hamnav";
   ham.id = "hamburger";
   tl.id = "topLine";
   ml.id = "middleLine";
   bl.id = "bottomLine";
 
+  ham.style.position = "absolute";
+
+  tl.style.position = "absolute";
   tl.style.width = "100%";
   tl.style.height = "20%";
   tl.style.top = "0%";
-  tl.style.backgroundColor = "red";
 
+  ml.style.position = "absolute";
   ml.style.width = "100%";
   ml.style.height = "20%";
   ml.style.top = "40%";
-  ml.style.backgroundColor = "red";
 
+  bl.style.position = "absolute";
   bl.style.width = "100%";
   bl.style.height = "20%";
   bl.style.top = "80%";
-  ml.style.backgroundColor = "red";
 
   ham.appendChild(tl);
+  ham.appendChild(ml);
+  ham.appendChild(bl);
   document.body.appendChild(ham);
+}
+
+// x = classcolor
+function setColors() {
+  if (hamburger.style.backgroundColor !== "transparent") {
+    topLine.style.backgroundColor = hamburger.style.backgroundColor;
+    middleLine.style.backgroundColor = hamburger.style.backgroundColor;
+    bottomLine.style.backgroundColor = hamburger.style.backgroundColor;
+    hamburger.style.backgroundColor = "transparent";
+  }
 }
